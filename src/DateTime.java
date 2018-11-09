6c102bf92989f349323b0ef8d8beb5a3dc674c03
Evestus <37637647+hovigstep@users.noreply.github.com>
2018-11-08 18:27:01 -0800

    Add files via upload

---
+++ src/DateTime.java
@@ -0,0 +1,40 @@
+
+public class DateTime {
+	int day;
+	int month;
+	int year;
+	int hour;
+	int minute;
+	int second;
+	
+
+public void day()
+{
+	
+}
+public void month()
+{
+	
+}
+
+public void year()
+{
+	
+}
+
+public void hour()
+{
+	
+}
+
+public void minute()
+{
+	
+}
+
+public void second()
+{
+	
+}
+
+}
---
+++ src/Datetimeserialized.java
@@ -0,0 +1,10 @@
+
+public class Datetimeserialized {
+	int day;
+	int month;
+	int year;
+	int hour;
+	int minute;
+	int second;
+	
+}
